4371e97426495f14c44908b8fcbcb41f31097286
LeonardoHGZ <142128438+LeonardoHGZ@users.noreply.github.com>
2023-10-27 20:48:15 -0600

    Add files via upload

---
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -0,0 +1,28 @@
+package com.formacionbdi.springboot.app.item.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formacionbdi.springboot.app.item.models.item;
+import com.formacionbdi.springboot.app.item.models.service.ItemService;
+
+@RestController
+public class ItemController {
+	@Autowired
+	@Qualifier("serviceFeign")
+	private ItemService itemService;
+	
+	@GetMapping("/listar")
+	public List<item> listar(){
+		return itemService.findAll();
+	}
+	@GetMapping("/var/{id}/cantidad/{cantidad}")
+	public item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemService.findById(id, cantidad);
+	}
+}
